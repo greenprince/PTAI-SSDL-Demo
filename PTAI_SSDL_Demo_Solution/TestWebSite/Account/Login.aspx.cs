fe26af38bab9fed1c60d82dd8232f163e59013b9
greenprince <federico.aureli@gmail.com>
2017-07-11 10:53:10 +0200

    [OR] in [Login.aspx.cs]

--- PTAI_SSDL_Demo_Solution/TestWebSite/Account/Login.aspx.cs
+++ PTAI_SSDL_Demo_Solution/TestWebSite/Account/Login.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Web;
 using System.Web.UI;
 using Microsoft.AspNet.Identity;
@@ -44,6 +44,14 @@ namespace TestWebSite.Account
                         Response.Redirect("/Account/Lockout");
                         break;
                     case SignInStatus.RequiresVerification:
+                        // TODO: AI issue #12, Medium, OR, http://ptssdal.ptsecurity.it.prv/#/taskResults/24
+                        // POST /Account/Login.aspx HTTP/1.1
+                        // Host: localhost
+                        // Content-Length: 103
+                        // Content-Type: application/x-www-form-urlencoded
+                        //
+                        // __AI_Button_mxyknblx=True&Email=ARBITRARY_DATA&Password=ARBITRARY_DATA&ReturnUrl=http%3a%2f%2flocalhost
+                        // (Microsoft.AspNet.Identity.Owin.SignInManagerExtensions.PasswordSignIn(Microsoft.AspNet.Identity.Owin.OwinContextExtensions.GetUserManager(System.Web.HttpContextExtensions.GetOwinContext(ASP.Page_Account_Login_aspx.Context)), System.Web.UI.WebControls.TextBox.Text, System.Web.UI.WebControls.TextBox.Text, false, false) == 2)
                         Response.Redirect(String.Format("/Account/TwoFactorAuthenticationSignIn?ReturnUrl={0}&RememberMe={1}", 
                                                         Request.QueryString["ReturnUrl"],
                                                         RememberMe.Checked),
